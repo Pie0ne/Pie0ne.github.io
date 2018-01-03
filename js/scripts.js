60ca79701fd0328602257b4b5961db6b62ac43f2
Pawel Kurzawa <pkurzawa90@gmail.com>
2018-01-03 21:41:45 +0100

    Create whole header section

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
+/* Scrool to bottom */
 
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
