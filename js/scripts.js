8355ab0e9e9d48f5239d836201d408638820e450
Pawel Kurzawa <pkurzawa90@gmail.com>
2018-01-08 16:41:16 +0100

    Change body font

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
-/* Scrool to bottom */
+/* scrool to bottom */
 
 $(function() {
   $('#section03').on('click', function() {
     $('html, body').animate({ 
     	scrollTop: $('#about').offset().top}, 1000);
-  });
+  	});
+  	
+  	/*collapsed menu */ 
 
     $('#button').click(function() {
         $('#menu').toggle('slow');
     });
+    
+    /* sliding to section */
 
     $('#homes').click(function() {
    		$('html, body').animate({ 
@@ -23,20 +27,27 @@ $(function() {
    		$('html, body').animate({ 
     	scrollTop: $('#skils').offset().top}, 1000);
    	});
+
+   	$('#workss').click(function() {
+   		$('html, body').animate({ 
+    	scrollTop: $('#works').offset().top}, 1000);
+   	});
+
+   	$('#contacts').click(function() {
+   		$('html, body').animate({ 
+    	scrollTop: $('#contact').offset().top}, 1000);
+   	});
 });
 
-/*skills*/
+/*skills rotate badges*/
 
 $(function(){
+	var $badges = $(".amg-badge img");
+	function rotateBadge(passedBadge, speed) {
+  		var rotateSpeed = speed / 180,
+      	current = 0;
 
-// Rotate Skill Badges
-var $badges = $(".amg-badge img");
-
-function rotateBadge(passedBadge, speed){
-  var rotateSpeed = speed / 180,
-      current = 0;
-
-  function badgeRotation(passedBadge){
+    function badgeRotation(passedBadge){
     current = (current == 90) ? 271 : current + 1;
 
     passedBadge.css({
@@ -48,20 +59,19 @@ function rotateBadge(passedBadge, speed){
     if (current == 360) {
       clearInterval(rotateInterval);
       passedBadge.removeAttr('style').removeClass('spinning');
-    }
-  }
+      }
+  	}
+  	var rotateInterval = setInterval(function(){badgeRotation(passedBadge)}, rotateSpeed);
+	}
 
-  var rotateInterval = setInterval(function(){badgeRotation(passedBadge)}, rotateSpeed);
-}
-
-$badges.mouseenter(function(){
-  if ($(this).hasClass('spinning')) {
-    return false;
-  } else {
-    rotateBadge($(this), 500);
-    $(this).addClass('spinning');
-  }
-});
-
+	$badges.mouseenter(function(){
+	  if ($(this).hasClass('spinning')) {
+	    return false;
+	  } 
+	  else {
+	    rotateBadge($(this), 500);
+	    $(this).addClass('spinning');
+	  }
+	});
 });
 
